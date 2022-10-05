8bdd39e92d6084160fdd238b328a36d3bc3e99b1
Nurribs <ayseibis50@gmail.com>
2022-10-05 12:16:40 +0300

    Examples

--- src/loopExamples/ucVeDordeTamBolunme.java
+++ src/loopExamples/ucVeDordeTamBolunme.java
@@ -2,8 +2,8 @@ package loopExamples;
 import java.util.Scanner;
 public class ucVeDordeTamBolunme {
     public static void main(String[] args) {
-        int j;
-        double toplam=0.0;
+        int j,sayac=0;
+        double toplam=0.0,ortalama=0.0;
 
         Scanner inp=new Scanner(System.in);
         System.out.print("Enter a number: ");
@@ -13,8 +13,12 @@ public class ucVeDordeTamBolunme {
             if (i%3==0 && i%4==0){
                 System.out.println(i);
                 toplam += i;
+                sayac++;
             }
         }
+        ortalama=toplam/sayac;
         System.out.println("Toplam: " +toplam);
+        System.out.println("Sayac: " +sayac);
+        System.out.println("Ortalama: " +ortalama);
     }
 }
