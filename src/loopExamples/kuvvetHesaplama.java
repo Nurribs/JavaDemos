92a17f415f381bd9354f1ef39667b52ac5d8e762
Nurribs <ayseibis50@gmail.com>
2022-10-05 16:56:11 +0300

    Examples

---
+++ src/loopExamples/kuvvetHesaplama.java
@@ -0,0 +1,17 @@
+package loopExamples;
+import java.util.Scanner;
+public class kuvvetHesaplama {
+    public static void main(String[] args) {
+        int n;
+        Scanner inp=new Scanner(System.in);
+        System.out.print("Sınır sayısını giriniz: ");
+        n=inp.nextInt();
+
+        for (int i=1; i<=n; i*=4){
+            System.out.println("i= " +i);
+        }
+        for (int j=1; j<=n; j*=5){
+            System.out.println("j= "+j);
+        }
+    }
+}
